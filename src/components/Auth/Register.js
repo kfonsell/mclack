b932f6784397ba56bc2ff315c49fb11652b2612c
Kimmo Fonsell <kimmo.fonsell@gmail.com>
2018-12-02 15:54:21 +0200

    adding registration functionality

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import firebase from "../../Firebase";
 import {
     Grid,
     Form,
@@ -8,51 +9,85 @@ import {
     Message,
     Icon
 } from "semantic-ui-react";
+import {
+    Link
+} from "react-router-dom";
 
 class Register extends React.Component {
-    state = {};
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    };
 
-    handleChange = () => {};
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(createdUser => {
+                console.log(createdUser);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
 
     render() {
+
+        const { username, email, password, passwordConfirmation } = this.state;
+
         return (
-            <Grid textAlign="center" verticalAlign="middle">
-                <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color="orange" textAlign="center">
-                        <Icon name="slack" color="orange" />
+            <Grid textAlign = "center" verticalAlign = "middle" className = "app">
+                <Grid.Column style = {{ maxWidth: 450 }}>
+                    <Header as = "h2" icon color = "orange" textAlign = "center">
+                        <Icon name = "slack" color = "orange" />
                         Register for MClack
                     </Header>
-                    <Form size="large">
+                    <Form onSubmit={this.handleSubmit} size = "large">
                         <Segment stacked>
-                            <Form.Input
-                                fluid
-                                name="username"
-                                icon="user"
-                                iconPosition="left"
-                                placeholder="Username"
-                                onChange={this.handleChange}
-                                type="text"
-                            />
-                            <Form.Input
-                                fluid
-                                name="email"
-                                icon="mail"
-                                iconPosition="left"
-                                placeholder="Email Address"
-                                onChange={this.handleChange}
-                                type="email"
-                            />
-                            <Form.Input
-                                fluid
-                                name="password"
-                                icon="lock"
-                                iconPosition="left"
-                                placeholder="Password"
-                                onChange={this.handleChange}
-                                type="password"
-                            />
-                        </Segment>
+                            <Form.Input fluid name = "username"
+                                        icon = "user"
+                                        iconPosition = "left"
+                                        placeholder = "Username"
+                                        onChange = { this.handleChange }
+                                        value = {username}
+                                        type = "text" />
+                            <Form.Input fluid name = "email"
+                                        icon = "mail"
+                                        iconPosition = "left"
+                                        placeholder = "Email Address"
+                                        onChange = { this.handleChange }
+                                        value = {email}
+                                        type = "email" />
+                            <Form.Input fluid name = "password"
+                                        icon = "lock"
+                                        iconPosition = "left"
+                                        placeholder = "Password"
+                                        onChange = { this.handleChange }
+                                        value = {password}
+                                        type = "password" />
+                            <Form.Input fluid name = "passwordConfirmation"
+                                        icon = "repeat"
+                                        iconPosition = "left"
+                                        placeholder = "Password Confirmation"
+                                        onChange = { this.handleChange }
+                                        value = {passwordConfirmation}
+                                        type = "password" />
+                            <Button color = "orange" fluid size = "large">Submit</Button>
+                        </Segment >
                     </Form>
+                    <Message>
+                        Already a user?
+                        <Link to = "/login">Login</Link>
+                    </Message>
                 </Grid.Column>
             </Grid>
         );

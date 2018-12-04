74b3fd19cad24f5b1a9b4168d3d28c27d816d3e0
kimmo.fonsell <kimmo.fonsell@mediconsult.fi>
2018-12-04 15:21:54 +0200

    adding redux and global user data functionality

--- src/index.js
+++ src/index.js
@@ -4,19 +4,59 @@ import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
+import firebase from "./Firebase";
 
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
 );
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
